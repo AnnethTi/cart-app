9d9403e3c21d8f9b5f6163afe172ff46f151c44b
Anneth Tinajero <annethtinajero@gmail.com>
2024-04-23 11:54:59 -0600

    Maquetado del carro de compras

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -4,7 +4,7 @@ export const CartApp = () => {
     <div className="container">
         <h3>Cart App</h3>
         <div className="row">
-            <div className="col">
+            <div className="col-4 my-2">
                 <div className="card">
                     <div className="card-body">
                         <h5 className="card-title"> Teclado Mecanico RGB</h5>
@@ -14,7 +14,7 @@ export const CartApp = () => {
                     </div>
                 </div>
             </div>
-            <div className="col">
+            <div className="col-4 my-2">
                 <div className="card">
                     <div className="card-body">
                         <h5 className="card-title"> Teclado Mecanico RGB</h5>
@@ -24,7 +24,7 @@ export const CartApp = () => {
                     </div>
                 </div>
             </div>
-            <div className="col">
+            <div className="col-4 my-2">
                 <div className="card">
                     <div className="card-body">
                         <h5 className="card-title"> Teclado Mecanico RGB</h5>
@@ -34,6 +34,36 @@ export const CartApp = () => {
                     </div>
                 </div>
             </div>
+        </div>
+
+        <div className="my-4 w-50">
+            <h3>Carro de Compra</h3>
+            <table className="table table-hover table-striped">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Precio</th>
+                        <th>Cantidad</th>
+                        <th>Total</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>nombre</td>
+                        <td>precio</td>
+                        <td>cantidad</td>
+                        <td>total</td>
+                        <td>eliminar</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan="3" className="text-end fw-bold">Total</td>
+                        <td colSpan="2" className="text-start fw-bold">$1234</td>
+                    </tr>
+                </tfoot>
+            </table>
         </div>
     </div>
     
