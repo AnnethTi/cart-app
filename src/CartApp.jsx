87dd62efbeda80b8b7db76817fa484d31bcbf6fc
Anneth Tinajero <annethtinajero@gmail.com>
2024-04-23 12:08:04 -0600

    El Service y el hook useEffect

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -1,71 +1,63 @@
+import { useEffect, useState } from "react";
+import { getProducts } from "./services/productService"
 
 export const CartApp = () => {
-  return (
-    <div className="container">
-        <h3>Cart App</h3>
-        <div className="row">
-            <div className="col-4 my-2">
-                <div className="card">
-                    <div className="card-body">
-                        <h5 className="card-title"> Teclado Mecanico RGB</h5>
-                        <p className="card-text">Teclado mecanico con luces rgb y switches red</p>
-                        <p className="card-text">$1000</p>
-                        <button className="btn btn-primary">Agregar</button>
+
+    const [products, setProducts] = useState([])
+
+
+    useEffect(() => {
+        setProducts(getProducts)
+    }, [])
+
+    return (
+        <div className="container">
+            <h3>Cart App</h3>
+            <div className="row">
+                {products.map(prod => (
+                    <div key={prod.id} className="col-4 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title"> {prod.name} </h5>
+                                <p className="card-text">{prod.description}</p>
+                                <p className="card-text">{prod.price}</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                ))}
             </div>
-            <div className="col-4 my-2">
-                <div className="card">
-                    <div className="card-body">
-                        <h5 className="card-title"> Teclado Mecanico RGB</h5>
-                        <p className="card-text">Teclado mecanico con luces rgb y switches red</p>
-                        <p className="card-text">$1000</p>
-                        <button className="btn btn-primary">Agregar</button>
-                    </div>
-                </div>
-            </div>
-            <div className="col-4 my-2">
-                <div className="card">
-                    <div className="card-body">
-                        <h5 className="card-title"> Teclado Mecanico RGB</h5>
-                        <p className="card-text">Teclado mecanico con luces rgb y switches red</p>
-                        <p className="card-text">$1000</p>
-                        <button className="btn btn-primary">Agregar</button>
-                    </div>
-                </div>
+
+            <div className="my-4 w-50">
+                <h3>Carro de Compra</h3>
+                <table className="table table-hover table-striped">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Precio</th>
+                            <th>Cantidad</th>
+                            <th>Total</th>
+                            <th>Eliminar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>nombre</td>
+                            <td>precio</td>
+                            <td>cantidad</td>
+                            <td>total</td>
+                            <td>eliminar</td>
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colSpan="3" className="text-end fw-bold">Total</td>
+                            <td colSpan="2" className="text-start fw-bold">$1234</td>
+                        </tr>
+                    </tfoot>
+                </table>
             </div>
         </div>
 
-        <div className="my-4 w-50">
-            <h3>Carro de Compra</h3>
-            <table className="table table-hover table-striped">
-                <thead>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Precio</th>
-                        <th>Cantidad</th>
-                        <th>Total</th>
-                        <th>Eliminar</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>nombre</td>
-                        <td>precio</td>
-                        <td>cantidad</td>
-                        <td>total</td>
-                        <td>eliminar</td>
-                    </tr>
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td colSpan="3" className="text-end fw-bold">Total</td>
-                        <td colSpan="2" className="text-start fw-bold">$1234</td>
-                    </tr>
-                </tfoot>
-            </table>
-        </div>
-    </div>
-    
-  )
+    )
 }
