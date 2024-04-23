e6765080d920e8cf75beb677e6cfd369f6b414fa
Anneth Tinajero <annethtinajero@gmail.com>
2024-04-23 15:52:11 -0600

    Navbar menu de navegacion

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -3,39 +3,40 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { CartView } from "./components/CartView";
 import { CatalogView } from "./components/CatalogView";
 import { useItemsCart } from "./hooks/useItemsCart";
+import { Navbar } from "./components/Navbar";
 
 export const CartApp = () => {
 
     const { cartItems, handlerAddProductCart, handlerDeleteProductCart } = useItemsCart();
 
     return (
-        <div className="container my-4">
-            <h3>Cart App</h3>
-            <Routes>
-                <Route
-                    path="catalog"
-                    element={<CatalogView handlerAdd={handlerAddProductCart} />}
-                />
-                <Route
-                    path="cart"
-                    element={(cartItems?.length <= 0 ?
-                        <div className="alert alert-warning">No hay productos en el carrito de compras</div>
-                        :
-                        (
-                        <div className="my-4 w-50">
-                            <CartView items={cartItems}
-                                handlerDelete={handlerDeleteProductCart}
-                            />
-                        </div>))}
-                />
-                <Route
-                    path="/"
-                    element={<Navigate to={'catalog'} />}
-                />
-
-            </Routes>
-
-        </div>
-
+        <>
+            <Navbar />
+            <div className="container my-4">
+                <h3>Cart App</h3>
+                <Routes>
+                    <Route
+                        path="catalog"
+                        element={<CatalogView handlerAdd={handlerAddProductCart} />}
+                    />
+                    <Route
+                        path="cart"
+                        element={(cartItems?.length <= 0 ?
+                            <div className="alert alert-warning">No hay productos en el carrito de compras</div>
+                            :
+                            (
+                                <div className="my-4 w-50">
+                                    <CartView items={cartItems}
+                                        handlerDelete={handlerDeleteProductCart}
+                                    />
+                                </div>))}
+                    />
+                    <Route
+                        path="/"
+                        element={<Navigate to={'catalog'} />}
+                    />
+                </Routes>
+            </div>
+        </>
     )
 }
