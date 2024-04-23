ab620edcca4a671dfe9d46baec6346b97d8909b5
Anneth Tinajero <annethtinajero@gmail.com>
2024-04-23 15:40:50 -0600

    Rutas con React Router

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -1,27 +1,39 @@
 
+import { Navigate, Route, Routes } from "react-router-dom";
 import { CartView } from "./components/CartView";
 import { CatalogView } from "./components/CatalogView";
 import { useItemsCart } from "./hooks/useItemsCart";
 
 export const CartApp = () => {
 
-    const {cartItems, handlerAddProductCart, handlerDeleteProductCart} = useItemsCart();
+    const { cartItems, handlerAddProductCart, handlerDeleteProductCart } = useItemsCart();
 
     return (
         <div className="container my-4">
             <h3>Cart App</h3>
-            <CatalogView
-                handlerAdd={handlerAddProductCart}
-            />
+            <Routes>
+                <Route
+                    path="catalog"
+                    element={<CatalogView handlerAdd={handlerAddProductCart} />}
+                />
+                <Route
+                    path="cart"
+                    element={(cartItems?.length <= 0 ?
+                        <div className="alert alert-warning">No hay productos en el carrito de compras</div>
+                        :
+                        (
+                        <div className="my-4 w-50">
+                            <CartView items={cartItems}
+                                handlerDelete={handlerDeleteProductCart}
+                            />
+                        </div>))}
+                />
+                <Route
+                    path="/"
+                    element={<Navigate to={'catalog'} />}
+                />
 
-            {cartItems?.length <= 0 || (
-
-                <div className="my-4 w-50">
-                    <CartView items={cartItems}
-                        handlerDelete={handlerDeleteProductCart}
-                    />
-                </div>
-            )}
+            </Routes>
 
         </div>
 
