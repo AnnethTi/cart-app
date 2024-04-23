f6f1201dc092ad3f7089b2cbf6db63e8c938ce95
Anneth Tinajero <annethtinajero@gmail.com>
2024-04-23 11:45:06 -0600

    Creando proyecto cart app

---
+++ src/CartApp.jsx
@@ -0,0 +1,41 @@
+
+export const CartApp = () => {
+  return (
+    <div className="container">
+        <h3>Cart App</h3>
+        <div className="row">
+            <div className="col">
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title"> Teclado Mecanico RGB</h5>
+                        <p className="card-text">Teclado mecanico con luces rgb y switches red</p>
+                        <p className="card-text">$1000</p>
+                        <button className="btn btn-primary">Agregar</button>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title"> Teclado Mecanico RGB</h5>
+                        <p className="card-text">Teclado mecanico con luces rgb y switches red</p>
+                        <p className="card-text">$1000</p>
+                        <button className="btn btn-primary">Agregar</button>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title"> Teclado Mecanico RGB</h5>
+                        <p className="card-text">Teclado mecanico con luces rgb y switches red</p>
+                        <p className="card-text">$1000</p>
+                        <button className="btn btn-primary">Agregar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+  )
+}
